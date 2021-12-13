6604558f005b8e1a13fde7d9895df39ae7a2862e
Arthur Magno <arthurmagno@arthurmagno.com>
2021-12-13 20:46:41 -0300

    Criação da estrutura

---
+++ menu-suspenso.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Menu Suspenso
+ * Plugin URI:      www.arthurmagno.com
+ * Description:     Menu suspenso
+ * Author:          Arthur Magno
+ * Author URI:      www.arthurmagno.com
+ * Text Domain:     menu-suspenso
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Menu_Suspenso
+ */
